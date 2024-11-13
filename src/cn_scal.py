f0cfab04a0d44af8a184f85b1ea452838141296a
Fabian Helmberger <e11810754@student.tuwien.ac.at>
2024-11-13 12:26:53 +0100

    scalar types

---
+++ src/cn_scal.py
@@ -0,0 +1,26 @@
+"""
+    Complex scalar functions and types
+"""
+
+import os
+import numpy as np
+
+# TODO: O(n) symmetry (complexified)
+
+# Degree of freedom: C^N
+N = 1
+
+scal_precision = os.environ.get('PRECISION', 'double')
+
+if scal_precision == 'single':
+    print("Using single precision")
+    SCAL_TYPE = np.complex64
+    SCAL_TYPE_REAL = np.float32
+
+elif scal_precision == 'double':
+    print("Using double precision")
+    SCAL_TYPE = np.complex128
+    SCAL_TYPE_REAL = np.float64
+    
+else:
+    print("Unsupported precision: " + scal_precision)
---
+++ src/rn_scal.py
@@ -0,0 +1,26 @@
+"""
+    Real scalar functions and types
+"""
+
+import os
+import numpy as np
+
+# TODO: O(n) symmetry
+
+# Degree of freedom: R^N
+N = 1
+
+scal_precision = os.environ.get('PRECISION', 'double')
+
+if scal_precision == 'single':
+    print("Using single precision")
+    SCAL_TYPE = np.float32
+    SCAL_TYPE_REAL = np.float32
+
+elif scal_precision == 'double':
+    print("Using double precision")
+    SCAL_TYPE = np.float64
+    SCAL_TYPE_REAL = np.float64
+    
+else:
+    print("Unsupported precision: " + scal_precision)
