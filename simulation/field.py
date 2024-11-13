1e118ac1c9d36a407953df1dfacce4e1af52e721
Fabian Helmberger <e11810754@student.tuwien.ac.at>
2024-11-13 15:56:56 +0100

    simulation sceleton

---
+++ simulation/field.py
@@ -0,0 +1,14 @@
+# simulation/field.py
+
+class Field:
+    def __init__(self, config):
+        self.config = config
+        self.initialize_field()
+
+    def initialize_field(self):
+        # Logic for initializing the scalar field
+        pass
+
+    def update_field(self, new_values):
+        # Update field values based on Langevin dynamics
+        pass
---
+++ simulation/langevin_dynamics.py
@@ -0,0 +1,10 @@
+# simulation/langevin_dynamics.py
+
+class LangevinDynamics:
+    def __init__(self, config, field):
+        self.config = config
+        self.field = field
+
+    def update(self):
+        # Perform Langevin update for the field
+        pass
