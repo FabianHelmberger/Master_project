c05b36cb76e25fed480712898fc71535bacb792d
Fabian Helmberger <e11810754@student.tuwien.ac.at>
2024-11-13 19:18:37 +0100

    renamed

---
+++ simulation/cl_simulation.py
@@ -0,0 +1,23 @@
+"""
+    Complex Langevin simulation module
+"""
+
+import numpy as np
+
+# from src.numba_target import use_cuda, myjit, my_parallel_loop, prange, threadsperblock
+
+from .field import *
+from .config import *
+from .langevin_dynamics import *
+from .observables import *
+from .lattice import *
+
+class ComplexLangevinSimulation:
+
+    def __init__(self, config):
+        
+        self.config = config
+        self.lattice = Lattice(self.config)
+        self.field = Field(self.lattice, self.config)
+        # self.dynamics = LangevinDynamics(self.config, self.field)
+        # self.observables = Observables(self.config, self.field)
