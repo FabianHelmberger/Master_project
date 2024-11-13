66950d7b5d93d098aef1d9b7f1e2718fd99fc5c8
Fabian Helmberger <e11810754@student.tuwien.ac.at>
2024-11-13 18:44:16 +0100

    lattice class with isolated methods (numba friendly)

---
+++ simulation/lattice.py
@@ -0,0 +1,20 @@
+import numpy as np
+import src.scal as scal
+
+import src.utils as utils
+
+class Lattice:
+    def __init__(self, dims):
+        self.dims = np.array(dims, dtype=scal.LATT_TYPE)
+        self.n_dims = len(self.dims)
+
+        cumprod_dims = np.cumprod(self.dims[::-1], dtype=scal.LATT_TYPE)[::-1]
+        self.adims = np.zeros(self.n_dims + 1, dtype=scal.LATT_TYPE)
+        self.adims[:-1] = cumprod_dims
+        self.adims[-1] = 1
+
+    def shift(self, index, dir, amount):
+        return utils.shift(index, dir, amount, self.adims, self.dims)
+
+    def get_index(self, pos):
+        return utils.get_index(pos, self.dims)
