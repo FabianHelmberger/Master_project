eeb1eed15bf81ccab5ab47dd6d6b14a5c39799c1
Fabian Helmberger <e11810754@student.tuwien.ac.at>
2024-11-15 18:50:35 +0100

    config includes mass and phi4 interaction

--- simulation/config.py
+++ simulation/config.py
@@ -6,6 +6,8 @@ class Config:
         self.steps = kwargs.get('steps', 1e3)
         self.dims = kwargs.get('dims', [10, 10])
         self.noise_factor = kwargs.get('noise_factor', 1.0)
-        self.dt = kwargs.get('dt', 1e-3)
+        self.dt = kwargs.get('dt', 1e-5)
         self.sqrt2 = kwargs.get('noise_factor', math.sqrt(2.0))
-        
+        self.mass_real = kwargs.get('mass_real', 1.0)
+        self.mass_imag = kwargs.get('mass_real', 0.0)
+        self.interaction = kwargs.get('interaction', 0.4)
