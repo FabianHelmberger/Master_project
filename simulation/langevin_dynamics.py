9f0520f869f95ef88a3cc7c8b19d27a988363309
Fabian Helmberger <e11810754@student.tuwien.ac.at>
2024-11-15 18:56:30 +0100

    addendum

--- simulation/langevin_dynamics.py
+++ simulation/langevin_dynamics.py
@@ -6,27 +6,45 @@ from .config import Config
 from .field import Field
 from .lattice import Lattice
 from src.numba_target import my_parallel_loop
-from src.utils import evolve_kernel
+from src.utils import euclidean_drift_kernel, mexican_hat_kernel_real, mexican_hat_kernel_complex
 
 class LangevinDynamics:
     def __init__(self, config: Config, field: Field, latt: Lattice):
         self.config = config
         self.field = field
         self.latt = latt
-
         self.sqrt2 = config.sqrt2
         self.noise_factor = self.config.noise_factor
         
     def update_noise(self):
         self.field.eta = self.noise_factor * self.sqrt2 * np.random.normal(size=self.field.eta.shape)
 
-    def update_field(self):
+    def update_drift(self):
+        # my_parallel_loop(
+        #     euclidean_drift_kernel,
+        #     self.latt.n_cells,
+        #     self.field.phi0,
+        #     self.latt.dims,
+        #     self.latt.adims,
+        #     self.field.dS,
+        #     self.config.mass_real,
+        #     self.config.mass_imag,
+        #     )
+
+        # my_parallel_loop(
+        #     mexican_hat_kernel_real,
+        #     self.latt.n_cells,
+        #     self.field.phi0,
+        #     self.field.dS,
+        #     self.config.mass_real,
+        #     self.config.interaction,
+        #     )
+
         my_parallel_loop(
-                    evolve_kernel,
-                    self.latt.n_cells,
-                    self.field.phi0,
-                    self.field.phi1,
-                    self.field.dS,
-                    self.field.eta,
-                    self.config.dt,
-                    )
+            mexican_hat_kernel_complex,
+            self.latt.n_cells,
+            self.field.phi0,
+            self.field.dS,
+            self.config.mass_real,
+            self.config.interaction,
+            )
