309ee7e7a1c5203b0e6f1e9cbacce99307bf0c2b
Fabian Helmberger <e11810754@student.tuwien.ac.at>
2024-11-27 14:47:05 +0100

    noise seed

--- simulation/config.py
+++ simulation/config.py
@@ -10,4 +10,5 @@ class Config:
         self.mass_real: scal.SCAL_TYPE_REAL = kwargs.get('mass_real', 1.0)
         self.mass_imag: scal.SCAL_TYPE_REAL = kwargs.get('mass_real', 0.0)
         self.interaction: scal.SCAL_TYPE_REAL = kwargs.get('interaction', 0.4)
+        self.noise_seed = 0
         super().__init__()  # Ensure compatibility with multiple inheritance
