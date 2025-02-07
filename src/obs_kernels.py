4b5433e30f187d901158c8af5b4becf539bbe913
Fabian Helmberger <e11810754@student.tuwien.ac.at>
2025-02-07 13:24:17 +0100

    dse moment obs kernel

--- src/obs_kernels.py
+++ src/obs_kernels.py
@@ -23,6 +23,42 @@ def n_moment_kernel(idx, phi0, result, order, langevin_time, adims, meas_time):
     meas_time[traj_idx] = langevin_time[traj_idx]
 
 @myjit
+def skew_action(idx, phi0, result, mass_real, interaction, langevin_time, adims, meas_time):
+    """
+    dyson-schwinger eq. predict, that the exp. value wrt. the unmodified theory of this skew action is zero
+    calculates sigma*phi+lambda*phi**3
+    """
+    phi_idx = phi0[idx]
+    res = mass_real * phi_idx*phi_idx + interaction * phi_idx*phi_idx*phi_idx*phi_idx - 1
+
+    result[idx] = res
+    traj_idx = idx // adims[1]
+    meas_time[traj_idx] = langevin_time[traj_idx]
+
+@myjit
+def dse_n_moment_kernel(idx, phi0, result, order, mass_real, interaction, langevin_time, adims, meas_time):
+    """
+    dyson-schwinger eq. predict, that the exp. value wrt. the unmodified theory of this skew action is one
+    calculates sigma*phi+lambda*phi**3
+    """
+    phi_idx = phi0[idx]
+    # phi_n = 1
+    # phi_n_1 = 1
+    # for k in range(order):
+    #     phi_n *= phi_idx
+    #     if k > 1 : phi_n_1 *= phi_idx
+    
+    res = order*phi_idx**(order-1)-phi_idx**order * (mass_real * phi_idx + interaction * phi_idx*phi_idx*phi_idx)
+
+    result[idx] = res
+    traj_idx = idx // adims[1]
+    meas_time[traj_idx] = langevin_time[traj_idx]
+
+
+
+
+
+@myjit
 def test_kernel(idx, result, constant_param_1):
     result[idx] = constant_param_1
 
