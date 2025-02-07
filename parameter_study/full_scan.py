97cd49c8a960ba98a2717bb0ddfc434b9c2c406c
Fabian Helmberger <e11810754@student.tuwien.ac.at>
2025-02-07 18:52:47 +0100

    scan in sbatch array

--- parameter_study/full_scan.py
+++ parameter_study/full_scan.py
@@ -38,12 +38,10 @@ def calculate_stats_complex(rolling_mean, rolling_sqr_mean, counter):
 
 
 
-
 import numpy as np
 
 # Set environment variables
 import os
-
 os.environ["SCAL_TYPE"] = "complex"
 os.environ["PRECISION"] = "single"
 os.environ["MY_NUMBA_TARGET"] = "cuda"
@@ -52,40 +50,14 @@ os.environ["MY_NUMBA_TARGET"] = "cuda"
 import sys
 sys.path.append("../../clonscal")
 
-# create parameters
-start_angle = 0
-stop_angle = 2 * np.pi
-angles_cl = np.linspace(start_angle, stop_angle, 16)
-
-pullback_phase = np.linspace(start_angle, stop_angle, )
-
-parameters = {
-    "steps": [int(1e3)],
-    "trajs": [int(1e4)],
-    "dt": [5e-3],
-    "sigma_abs": [1],
-    "sigma_phase": angles_cl,
-    "lambda_abs": [1],
-    "pullback_phase": [0],
-    "pullback_abs": [5],
-    "mass_modification": [2],
-}
-
-# Generate all parameter combinations
-import itertools
-param_combinations = [dict(zip(parameters.keys(), values)) for values in itertools.product(*parameters.values())]
-
-
-# Running the simulation
+# defining the simulation
 from simulation.config import Config
 from simulation.cl_simulation import ComplexLangevinSimulation
+from simulation.gpu_handler import GPU_handler
 from src.obs_kernels import n_moment_kernel, dse_n_moment_kernel
 from src.utils import gaussian_modified_density_drift_kernel
-from tqdm import tqdm
-
-
-from simulation.gpu_handler import GPU_handler
 from src.numba_target import use_cuda
+from tqdm import tqdm
 
 def run_sim(params):
     param_key = "_".join(map(str, params.values()))
@@ -132,10 +104,45 @@ def run_sim(params):
     return results, param_key
 
 
-for param in param_combinations:
-    results, param_key = run_sim(param)
+# create parameters
+start_angle = 0
+stop_angle = 2 * np.pi
+angles_cl = np.linspace(start_angle, stop_angle, 16)
+pullback_phase = np.linspace(start_angle, stop_angle)
+parameters = {
+    "steps": [int(5e4)],
+    "trajs": [int(1e4)],
+    "dt": [5e-4],
+    "sigma_abs": [1],
+    "sigma_phase": angles_cl,
+    "lambda_abs": [1],
+    "pullback_phase": [0],
+    "pullback_abs": [5],
+    "mass_modification": [2],
+}
 
-    import json
-    file_path = os.path.join(os.getenv("HOME"), "gitrepos", "clonscal", "parameter_study", "sim_data", f"{param_key}.json")
-    with open(file_path, "w") as f:
-        json.dump(results, f, indent=4)
+# Generate all parameter combinations
+import itertools
+param_combinations = [dict(zip(parameters.keys(), values)) for values in itertools.product(*parameters.values())]
+
+# Get index from Slurm
+slurm_index = int(os.getenv("SLURM_ARRAY_TASK_ID", "0"))  # Default to 0 if not running in Slurm
+
+if slurm_index >= len(param_combinations):
+    print(f"Invalid SLURM_ARRAY_TASK_ID={slurm_index}, max is {len(param_combinations)-1}")
+    sys.exit(1)
+
+# Select parameters for this job
+param = param_combinations[slurm_index]
+
+
+# Run the simulation
+results, param_key = run_sim(param)
+
+# Save results
+import json
+file_path = os.path.join(os.getenv("HOME"), "gitrepos", "clonscal", "parameter_study", "sim_data", f"{param_key}.json")
+with open(file_path, "w") as f:
+    json.dump(results, f, indent=4)
+
+print(f"{slurm_index}: Saved results to {file_path}")
