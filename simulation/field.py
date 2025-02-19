1ef00ae37f60b3d06e3d708dfc0b4ee0e7dd9920
Fabian Helmberger <e11810754@student.tuwien.ac.at>
2025-02-19 15:19:27 +0100

    random phi start

--- simulation/field.py
+++ simulation/field.py
@@ -11,8 +11,14 @@ class Field(Lattice):
     def __init__(self, config):
         super().__init__(config)
 
+        # self.phi0 = scal.SCAL_TYPE(np.random.normal(self.n_cells))
+        # self.phi1 = scal.SCAL_TYPE(np.random.normal(self.n_cells))
+
         self.phi0 = np.zeros(self.n_cells, dtype=scal.SCAL_TYPE)
         self.phi1 = np.zeros(self.n_cells, dtype=scal.SCAL_TYPE)
+        # rand = np.random.normal(size=(4, 2)).astype(scal.SCAL_TYPE)
+        # self.phi0 = rand[0,:]
+        # self.phi1 = rand[2,:]
 
     def swap(self):
         self.phi0, self.phi1 = self.phi1, self.phi0
