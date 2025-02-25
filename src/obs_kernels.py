41efea54a06e7dfbe7236044b11e1e673529d8be
Fabian Helmberger <e11810754@student.tuwien.ac.at>
2025-02-26 00:23:10 +0100

    sigma relabel

--- src/obs_kernels.py
+++ src/obs_kernels.py
@@ -31,20 +31,20 @@ def abs_drift(idx, result, dS_norm, langevin_time, meas_time):
 
 
 @myjit
-def skew_action(idx, phi0, result, mass_real, interaction, langevin_time, adims, meas_time):
+def skew_action(idx, phi0, result, sigma, interaction, langevin_time, adims, meas_time):
     """
     dyson-schwinger eq. predict, that the exp. value wrt. the unmodified theory of this skew action is zero
     calculates sigma*phi+lambda*phi**3
     """
     phi_idx = phi0[idx]
-    res = mass_real * phi_idx*phi_idx + interaction * phi_idx*phi_idx*phi_idx*phi_idx - 1
+    res = sigma * phi_idx*phi_idx + interaction * phi_idx*phi_idx*phi_idx*phi_idx - 1
 
     result[idx] = res
     traj_idx = idx // adims[1]
     meas_time[traj_idx] = langevin_time[traj_idx]
 
 @myjit
-def dse_n_moment_kernel(idx, phi0, result, order, mass_real, interaction, langevin_time, adims, meas_time):
+def dse_n_moment_kernel(idx, phi0, result, order, sigma, interaction, langevin_time, adims, meas_time):
     """
     dyson-schwinger eq. predict, that the exp. value wrt. the unmodified theory of this skew action is one
     calculates sigma*phi+lambda*phi**3
@@ -56,12 +56,11 @@ def dse_n_moment_kernel(idx, phi0, result, order, mass_real, interaction, langev
     #     phi_n *= phi_idx
     #     if k > 1 : phi_n_1 *= phi_idx
     
-    res = order*phi_idx**(order-1)-phi_idx**order * (mass_real * phi_idx + interaction * phi_idx*phi_idx*phi_idx)
+    res = order*phi_idx**(order-1)-phi_idx**order * (sigma * phi_idx + interaction * phi_idx*phi_idx*phi_idx)
 
     result[idx] = res
     traj_idx = idx // adims[1]
     meas_time[traj_idx] = langevin_time[traj_idx]
-
 
 
 
