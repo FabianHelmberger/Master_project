735ef9f82e618e31b617f26fe12da91ef5e56205
Fabian Helmberger <e11810754@student.tuwien.ac.at>
2025-03-15 12:15:22 +0100

    renamed int type

--- simulation/lattice.py
+++ simulation/lattice.py
@@ -9,13 +9,13 @@ class Lattice(Config, Constants):
     def __init__(self, config: Config):
         super().__init__(**config.__dict__)  # Use super() to handle both parent initializations
         self.config = config
-        self.dims = np.array(self.dims, dtype=scal.LATT_TYPE)
+        self.dims = np.array(self.dims, dtype=scal.IDX_TYPE)
         self.sim_dims = np.append(self.trajs, self.dims) # artificial lattice including direction for different trajs
         self.n_dims = len(self.sim_dims)
         self.n_cells = int(np.prod(self.sim_dims))
 
-        cumprod_dims = np.cumprod(self.sim_dims[::-1], dtype=scal.LATT_TYPE)[::-1]
-        self.adims = np.zeros(self.n_dims + 1, dtype=scal.LATT_TYPE)
+        cumprod_dims = np.cumprod(self.sim_dims[::-1], dtype=scal.IDX_TYPE)[::-1]
+        self.adims = np.zeros(self.n_dims + 1, dtype=scal.IDX_TYPE)
         self.adims[:-1] = cumprod_dims
         self.adims[-1] = 1
 
